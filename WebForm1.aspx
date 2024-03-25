fe8b2ca7be967421e8be36fdb505953508f3a391
mdsab <mdsab@DESKTOP-SSMFGL7>
2024-03-25 11:28:02 +0600

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RoboButton2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+         <div>
+     <asp:Button ID="OFFButton" runat="server" Text="OFF" BackColor="Lime" Font-Bold="True" Width="51px" OnClick="OFFButton_Click" />
+     <asp:Button ID="ONButton" runat="server" Text="ON" BackColor="#FF3300" Font-Bold="True" Width="51px" OnClick="ONButton_Click" />
+ </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO.Ports;
+
+namespace RoboButton2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private SerialPort serialPort;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            serialPort = new SerialPort("COM4", 9600); // Change COM3 to match your Arduino's COM port
+            serialPort.Open();
+        }
+
+        protected void OFFButton_Click(object sender, EventArgs e)
+        {
+            serialPort.Write("0");
+        }
+
+        protected void ONButton_Click(object sender, EventArgs e)
+        {
+            serialPort.Write("1");
+        }
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            // Close the serial port when the page is unloaded
+            serialPort.Close();
+        }
+    }
+}
